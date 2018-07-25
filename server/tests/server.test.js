5042bbbfc96b65c2d29c451dd0deaf8e589e3582
janisozo <todlebensjanis@gmail.com>
2018-07-25 10:12:49 -0700

    added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -229,7 +229,7 @@ describe('POST /users', () => {
 					expect(user).toBeTruthy();
 					expect(user.password).not.toBe(password);
 					done();
-				});
+				}).catch((e) => done(e));
 			});
 	});
 
@@ -263,5 +263,55 @@ describe('POST /users', () => {
 			.end(done);
 		// use a used email (seed data)
 		// expect (400)
+	});
+});
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: users[1].password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.header['x-auth']).toBeTruthy();
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				user.findById(users[1]._id).then((user) => {
+					expect(user.tokens[0]).toMatchObject({
+						access: 'auth',
+						token: res.header['x-auth']
+					});
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should reject invalid login', (done) => {
+		request(app)
+			.post('/users/login')
+			.send({
+				email: users[1].email,
+				password: 'incorrect'
+			})
+			.expect(401)
+			.expect((res) => {
+				expect(res.header['x-auth']).toBeFalsy();
+			})
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+				user.findById(users[1]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+		});
 	});
 });
